54dbc8d3ca2bdc46f159553587c4aaccbde2fb30
wytzz <wytzeaz@hotmail.com>
2018-12-03 14:29:52 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Wytze Dijkstra on 03/12/2018.
+//  Copyright © 2018 Wytze Dijkstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
